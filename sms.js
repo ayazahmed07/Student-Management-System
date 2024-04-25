f52cc30eaef480e7da41143dcf58cd0eaaa7d8bb
Ayaz ahmed <ayazahmed824@gmail.com>
2024-04-25 09:20:27 +0000

    first commit

---
+++ sms.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+let new_students = [];
+let add_students = await inquirer.prompt([{
+        name: "addstudent",
+        message: "What is the name of student?",
+        type: "input",
+    },
+    {
+        name: "course",
+        message: "Please select the course for enrolment",
+        type: "list",
+        choices: ["English Language", "Computer Programming", "Artificail Intelligence", "Machine Learning", "Data Science"]
+    }
+]);
+new_students.push(add_students.addstudent);
+new_students.forEach((new_students) => console.log(`${new_students} is enrolled in ${add_students.course} course and his ID is generateuniqueid`));
