eb35f9b56c299e5ed8e59e932df98f37de7a8467
Ayaz ahmed <ayazahmed824@gmail.com>
2024-05-16 11:48:46 +0000

    modified

--- sms.js
+++ sms.js
@@ -3,7 +3,7 @@ import chalk from "chalk";
 async function textanimation(text) {
     for (let char of text) {
         process.stdout.write(char);
-        await new Promise((resolve) => setTimeout(resolve, 50));
+        await new Promise((resolve) => setTimeout(resolve, 15));
     }
 }
 async function exitanimation(text) {
@@ -12,9 +12,10 @@ async function exitanimation(text) {
         await new Promise((resolve) => setTimeout(resolve, 10));
     }
 }
-textanimation(chalk.bold.red.bgBlue("\t\t\t Welcome in Student Managemet System\n"));
-let selectoption = await inquirer.prompt([{
-        name: "addstudent",
+await textanimation(chalk.bold.red.bgBlue("\t\t\t Welcome in Student Managemet System\n"));
+let selectoption = await inquirer.prompt([
+    {
+        name: "smsform",
         message: "Please select any one option",
         type: "list",
         choices: [
@@ -25,5 +26,100 @@ let selectoption = await inquirer.prompt([{
             "Pay Fee",
             "View Balance",
             "Enroll Student",
-        ]
-    }]);
+        ],
+    },
+]);
+let studentdata = [];
+let condition = true;
+if (selectoption.smsform === "Add New Student")
+    while (condition) {
+        {
+            let addnewstudent = await inquirer.prompt([
+                {
+                    name: "newstudent",
+                    message: "Please type student complete name?",
+                    type: "input",
+                    validate: (input) => {
+                        if (!input.trim()) {
+                            return "Please enter name to continue...";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    name: "fathername",
+                    message: "Please type student's father name?",
+                    type: "input",
+                    validate: (input) => {
+                        if (!input.trim()) {
+                            return "Please enter name to continue...";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    name: "age",
+                    message: "Please type student age?",
+                    type: "input",
+                    validate: (input) => {
+                        if (!input.trim()) {
+                            return "Please enter age...";
+                        }
+                        const age = parseInt(input);
+                        if (isNaN(age)) {
+                            return "Please enter a valid number for age..";
+                        }
+                        if (age > 99) {
+                            return "Age should not be greater than 99";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    name: "gender",
+                    message: "Please select gender?",
+                    type: "list",
+                    choices: ["Male", "Female"],
+                },
+                {
+                    name: "contact",
+                    message: "Please enter contact details? (XXXX-XXXXXXX)",
+                    type: "input",
+                    validate: (input) => {
+                        if (!input.trim()) {
+                            return "Please enter contact details...";
+                        }
+                        const contact = parseInt(input);
+                        if (isNaN(contact)) {
+                            return "Please enter a valid contact number..";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    name: "cnic",
+                    message: "Please enter CNIC? (XXXXX-XXXXXXX-X)",
+                    type: "input",
+                    validate: (input) => {
+                        if (!input.trim()) {
+                            return "Please enter CNIC...";
+                        }
+                        const cnic = parseInt(input);
+                        if (isNaN(cnic)) {
+                            return "Please enter a valid CNIC number..";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    name: "askquestion",
+                    type: "confirm",
+                    message: chalk.blueBright.bold("Would you like to add more students"),
+                    default: "true",
+                },
+            ]);
+            condition = addnewstudent.askquestion;
+        }
+    }
+studentdata.push(selectoption.addnewstudent);
+console.log(studentdata);
--- sms.ts
+++ sms.ts
@@ -1,11 +1,13 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+let studentdata: any[] = [];
+
 async function textanimation(text: string) {
   for (let char of text) {
     process.stdout.write(char);
 
-    await new Promise((resolve) => setTimeout(resolve, 50));
+    await new Promise((resolve) => setTimeout(resolve, 15));
   }
 }
 
@@ -17,22 +19,137 @@ async function exitanimation(text: string) {
   }
 }
 
-await textanimation(chalk.bold.red.bgBlue("\t\t\t Welcome in Student Managemet System\n"));
+await textanimation(
+  chalk.bold.red.bgBlue("\t\t\t Welcome in Student Managemet System\n")
+);
+
+let selectoption = await inquirer.prompt([
+  {
+    name: "smsform",
+    message: "Please select any one option",
+    type: "list",
+    choices: [
+      "Add New Student",
+      "View Students",
+      "Delete Student",
+      "View Courses and Fee",
+      "Pay Fee",
+      "View Balance",
+      "Enroll Student",
+    ],
+  },
+]);
 
 
-let selectoption = await inquirer.prompt([{
-name: "addstudent",
-message: "Please select any one option",
-type: "list",
-choices: [
-  "Add New Student",
-  "View Students",
-  "Delete Student",
-  "View Courses and Fee",
-  "Pay Fee",
-  "View Balance",
-  "Enroll Student",
+let condition = true;
 
-]
+if (selectoption.smsform === "Add New Student")
+  while (condition) {
+    {
+      let addnewstudent = await inquirer.prompt([
+        {
+          name: "newstudent",
+          message: "Please type student complete name?",
+          type: "input",
+          validate: (input) => {
+            if (!input.trim()) {
+              return "Please enter name to continue...";
+            }
 
-}])
+            return true;
+          },
+        },
+
+        {
+          name: "fathername",
+          message: "Please type student's father name?",
+          type: "input",
+          validate: (input) => {
+            if (!input.trim()) {
+              return "Please enter name to continue...";
+            }
+
+            return true;
+          },
+        },
+
+        {
+          name: "age",
+          message: "Please type student age?",
+          type: "input",
+          validate: (input) => {
+            if (!input.trim()) {
+              return "Please enter age...";
+            }
+
+            const age = parseInt(input);
+            if (isNaN(age)) {
+              return "Please enter a valid number for age..";
+            }
+
+            if (age > 99) {
+              return "Age should not be greater than 99";
+            }
+
+            return true;
+          },
+        },
+
+        {
+          name: "gender",
+          message: "Please select gender?",
+          type: "list",
+          choices: ["Male", "Female"],
+        },
+
+        {
+          name: "contact",
+          message: "Please enter contact details? (XXXX-XXXXXXX)",
+          type: "input",
+          validate: (input) => {
+            if (!input.trim()) {
+              return "Please enter contact details...";
+            }
+
+            const contact = parseInt(input);
+            if (isNaN(contact)) {
+              return "Please enter a valid contact number..";
+            }
+
+            return true;
+          },
+        },
+
+        {
+          name: "cnic",
+          message: "Please enter CNIC? (XXXXX-XXXXXXX-X)",
+          type: "input",
+          validate: (input) => {
+            if (!input.trim()) {
+              return "Please enter CNIC...";
+            }
+
+            const cnic = parseInt(input);
+            if (isNaN(cnic)) {
+              return "Please enter a valid CNIC number..";
+            }
+
+            return true;
+          },
+        },
+
+        {
+          name: "askquestion",
+          type: "confirm",
+          message: chalk.blueBright.bold("Would you like to add more students"),
+          default: "true",
+        },
+      ]);
+      condition = addnewstudent.askquestion;
+    }
+  }
+
+  studentdata.push(addnewstudent);
+
+ 
+
